d5429ed09f5101ee4de589c0a7076a93717202b2
hhh <957400829@qq.com>
2018-03-13 21:24:45 +0800

    Initial commit

---
+++ src/util/TestAbstracCollection.java
@@ -0,0 +1,11 @@
+package util;
+
+/**
+ * Created by xiongxiaoyu on 2018/3/13.
+ */
+public class TestAbstracCollection {
+
+	public static void main(String[] args) {
+		System.out.println("JHHHH");
+	}
+}
