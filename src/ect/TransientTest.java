8905e353b0d090da429bf9e4dbd48f084c9107f2
hhh <957400829@qq.com>
2018-03-17 20:14:43 +0800

    add collection test

--- src/ect/TransientTest.java
+++ src/ect/TransientTest.java
@@ -7,6 +7,8 @@ import java.io.*;
  * 使用transient关键字不序列化某个变量
  *   读取的时候，读取数据的顺序一定和存放的顺序保持一致
  * 1.一旦变量被transient修饰，改变量不是对象持久化的一部分，在序列化后无法再次访问
+ * 2.transient只能修饰变量，不能修饰方法和类且应实现Serializable接口，本地变量也不能被其修饰。
+ * 3.被改关键字修饰的变量和静态变量均不能被序列化。
  */
 
 public class TransientTest {
@@ -14,7 +16,7 @@ public class TransientTest {
 	public static void main(String[] args) {
 
 		User user = new User();
-		user.setUsername("Alexia");
+		user.setUsername("ABC");
 		user.setPasswd("123456");
 
 		System.out.println("read before Serializable: ");
