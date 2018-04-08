b09c02ff47b7c1a194f15323db0374080457f82d
hhh <957400829@qq.com>
2018-04-08 21:57:42 +0800

    add enum map

--- src/util/TestEnum.java
+++ src/util/TestEnum.java
@@ -9,7 +9,13 @@ import java.util.Map;
  * Time:21:05
  *
  * 抽象类Enum，Enum自带的compareTo()返回的序列号差值self.ordinal - other.ordinal,
- * EnumMap是专门和枚举类结合形成Map的key-value键值对结构，内存实现是数组结构
+ *
+ * EnumMap是专门和枚举类结合形成Map的key-value键值对结构，内存实现是数组结构的Map实现。
+ * 		（同一类型枚举类型键值实例到值的映射，由于枚举类型相对固定数量少，通过枚举类型
+ * 		的ordinal()获得当前实例的声明次序，来维持实例字数组中的位置，EnumMap更加高效）
+ *		key不能null，value可以null（实际maskNull后,以Null存在数据库中）
+ *可用工具类Collections包装成线程安全的：
+ * 		Map<EnumKey, V> m = Collections.synchronizedMap(new EnumMap<EnumKey, V>();
  *
  */
 public class TestEnum {
@@ -19,7 +25,7 @@ public class TestEnum {
 	}
 	public static void main(String[] args) {
 
-		Map<Operate,String> map=new EnumMap<Operate, String>(Operate.class);
+		Map<Operate,String> map=new EnumMap<>(Operate.class);
 		//put方法
 		map.put(Operate.ADD, "add operate");
 		map.put(Operate.UPDATE, "update operate");
