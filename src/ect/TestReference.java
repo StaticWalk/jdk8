e31f9a6c1132f88e67306ad39ff22bf260dbb1dc
hhh <957400829@qq.com>
2018-04-10 16:02:41 +0800

    add Reference

--- src/ect/TestReference.java
+++ src/ect/TestReference.java
@@ -1,4 +1,6 @@
 package ect;
+import java.util.WeakHashMap;
+
 
 /**
  * Created by xiongxiaoyu
@@ -14,6 +16,56 @@ package ect;
  *		对象可达性事件处理的场景会使用。(Reference指引用对象本身，Referent指引用对象)
  *   对象可达性判断
  */
-public class TestReference
-{
+
+
+class Element {
+private String ident;
+
+public Element(String id) {
+		ident = id;
+		}
+
+public String toString() {
+		return ident;
+		}
+
+public int hashCode() {
+		return ident.hashCode();
+		}
+
+public boolean equals(Object obj) {
+		return obj instanceof Element && ident.equals(((Element) obj).ident);
+		}
+
+protected void finalize(){
+		System.out.println("Finalizing "+getClass().getSimpleName()+" "+ident);
+		}}
+
+class Key extends Element{
+	public Key(String id){
+		super(id);
+	}
+}
+
+class Value extends Element{
+	public Value (String id){
+		super(id);
+	}
+}
+
+public class TestReference {
+	public static void main(String[] args) {
+		int size = 1000;
+		Key[] keys = new Key[size];
+		WeakHashMap map = new WeakHashMap();
+		for (int i = 0; i < keys.length; i++)
+		{
+			Key k = new Key(Integer.toString(i));
+			Value v = new Value(Integer.toString(i));
+			if (i % 3 == 0)
+				keys[i] = k;
+			map.put(k, v);
+		}
+		System.gc();
+	}
 }
